49e824be07a49057049ac57c7a94c1d9afab022f
kitamin <11195207+meian@users.noreply.github.com>
2022-01-22 00:16:28 +0900

    #12 Add FlatMap implements

--- map_test.go
+++ map_test.go
@@ -68,3 +68,84 @@ func ExampleMap() {
 	// mouse:5
 	// rabbit:6
 }
+
+func TestFlatMap(t *testing.T) {
+	mf := func(v string) []int {
+		r := make([]int, 0)
+		for i := 0; i < len(v); i++ {
+			r = append(r, i+1)
+		}
+		return r
+	}
+	mfe := func(v string) []int {
+		return make([]int, 0)
+	}
+	mfn := func(v string) []int {
+		return nil
+	}
+	tests := []struct {
+		name    string
+		itb     gcf.Iterable[string]
+		mapFunc func(v string) []int
+		want    []int
+	}{
+		{
+			name:    "normal",
+			itb:     gcf.FromSlice([]string{"a", "bc", "def", "ghij"}),
+			mapFunc: mf,
+			want:    []int{1, 1, 2, 1, 2, 3, 1, 2, 3, 4},
+		},
+		{
+			name:    "empty iterable",
+			itb:     gcf.FromSlice([]string{}),
+			mapFunc: mf,
+			want:    []int{},
+		},
+		{
+			name:    "nil iterable",
+			itb:     nil,
+			mapFunc: mf,
+			want:    []int{},
+		},
+		{
+			name:    "empty return func",
+			itb:     gcf.FromSlice([]string{"a", "bc", "def", "ghij"}),
+			mapFunc: mfe,
+			want:    []int{},
+		},
+		{
+			name:    "nil return func",
+			itb:     gcf.FromSlice([]string{"a", "bc", "def", "ghij"}),
+			mapFunc: mfn,
+			want:    []int{},
+		},
+		{
+			name:    "nil func",
+			itb:     gcf.FromSlice([]string{"a", "bc", "def", "ghij"}),
+			mapFunc: nil,
+			want:    []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			itbi := gcf.FlatMap(tt.itb, tt.mapFunc)
+			s := gcf.ToSlice(itbi)
+			assert.Equal(t, tt.want, s)
+		})
+	}
+}
+
+func ExampleFlatMap() {
+	s := []string{"dog", "cat", "mouse", "rabbit"}
+	itb := gcf.FromSlice(s)
+	itbs := gcf.FlatMap(itb, func(v string) []string {
+		r := make([]string, 0, len(v))
+		for _, c := range []rune(v) {
+			r = append(r, fmt.Sprintf("%c", c))
+		}
+		return r
+	})
+	fmt.Println(gcf.ToSlice(itbs))
+	// Output:
+	// [d o g c a t m o u s e r a b b i t]
+}
