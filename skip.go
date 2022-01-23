9e54770f24e15fa5a19f41703cd963b359c7dac3
kitamin <11195207+meian@users.noreply.github.com>
2022-01-24 03:49:57 +0900

    #22 Add SkipWhile implements

--- skip.go
+++ skip.go
@@ -47,3 +47,58 @@ func (it *skipIterator[T]) MoveNext() bool {
 func (it *skipIterator[T]) Current() T {
 	return it.current
 }
+
+type skipWhileIterable[T any] struct {
+	itb       Iterable[T]
+	whileFunc func(v T) bool
+}
+
+type skipWhileIterator[T any] struct {
+	it        Iterator[T]
+	whileFunc func(v T) bool
+	skipDone  bool
+	current   T
+}
+
+// SkipWhile makes Iterable with elements excepting elements that whileFunc is true from ahead.
+//
+//   itb := gcf.FromSlice([]{1, 2, 3})
+//   itb = gcf.SkipWhile(itb, func(v int) bool { return v <= 2 })
+//
+// If whileFunc is nil, returns original Iterable.
+func SkipWhile[T any](itb Iterable[T], whileFunc func(v T) bool) Iterable[T] {
+	if itb == nil {
+		return empty[T]()
+	}
+	if whileFunc == nil {
+		return itb
+	}
+	return &skipWhileIterable[T]{itb, whileFunc}
+}
+
+func (itb *skipWhileIterable[T]) Iterator() Iterator[T] {
+	return &skipWhileIterator[T]{itb.itb.Iterator(), itb.whileFunc, false, zero[T]()}
+}
+
+func (it *skipWhileIterator[T]) MoveNext() bool {
+	if !it.skipDone {
+		for it.it.MoveNext() {
+			if it.whileFunc(it.it.Current()) {
+				continue
+			}
+			it.skipDone = true
+			it.current = it.it.Current()
+			return true
+		}
+	}
+	if it.it.MoveNext() {
+		it.current = it.it.Current()
+		return true
+	}
+	it.current = zero[T]()
+	return false
+}
+
+func (it *skipWhileIterator[T]) Current() T {
+	return it.current
+}
--- skip_test.go
+++ skip_test.go
@@ -95,3 +95,84 @@ func ExampleSkip() {
 	// Output:
 	// [3 4 5]
 }
+
+func TestSkipWhile(t *testing.T) {
+	type args struct {
+		itb       gcf.Iterable[int]
+		whileFunc func(v int) bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "all true",
+			args: args{
+				itb:       gcf.FromSlice([]int{1, 2, 3}),
+				whileFunc: func(v int) bool { return true },
+			},
+			want: []int{},
+		},
+		{
+			name: "all false",
+			args: args{
+				itb:       gcf.FromSlice([]int{1, 2, 3}),
+				whileFunc: func(v int) bool { return false },
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "partial true from ahead",
+			args: args{
+				itb:       gcf.FromSlice([]int{1, 2, 3}),
+				whileFunc: func(v int) bool { return v <= 2 },
+			},
+			want: []int{3},
+		},
+		{
+			name: "partial true not from ahead",
+			args: args{
+				itb:       gcf.FromSlice([]int{1, 2, 3}),
+				whileFunc: func(v int) bool { return v >= 2 },
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "nil func",
+			args: args{
+				itb:       gcf.FromSlice([]int{1, 2, 3}),
+				whileFunc: nil,
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "nil Iterable",
+			args: args{
+				itb:       nil,
+				whileFunc: func(v int) bool { return true },
+			},
+			want: []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			itb := gcf.SkipWhile(tt.args.itb, tt.args.whileFunc)
+			s := gcf.ToSlice(itb)
+			assert.Equal(t, tt.want, s)
+		})
+	}
+
+	itb := gcf.FromSlice([]int{1, 2, 3})
+	itb = gcf.SkipWhile(itb, func(v int) bool { return v < 2 })
+	testBeforeAndAfter(t, itb)
+}
+
+func ExampleSkipWhile() {
+	itb := gcf.FromSlice([]int{1, 3, 5, 7, 2, 4, 6, 8, 9})
+	itb = gcf.SkipWhile(itb, func(v int) bool { return v%2 > 0 })
+	s := gcf.ToSlice(itb)
+	fmt.Println(s)
+	// Output:
+	// [2 4 6 8 9]
+}
