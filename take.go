8f053173a9d024cd91458d137a95b034088f795a
kitamin <11195207+meian@users.noreply.github.com>
2022-01-23 17:15:59 +0900

    #21 Add TakeWhile Implements

--- take.go
+++ take.go
@@ -55,3 +55,52 @@ func (it *takeIterator[T]) MoveNext() bool {
 func (it *takeIterator[T]) Current() T {
 	return it.current
 }
+
+type takeWhileIterable[T any] struct {
+	itb       Iterable[T]
+	whileFunc func(v T) bool
+}
+
+type takeWhileIterator[T any] struct {
+	it        Iterator[T]
+	whileFunc func(v T) bool
+	done      bool
+	current   T
+}
+
+// TakeWhile makes Iterable with elements in which whileFunc is true from ahead.
+//
+//   itb := gcf.FromSlice([]{1, 2, 3})
+//   itb = gcf.TakeWhile(itb, func(v int) bool { return v <= 2 })
+//
+// If whileFunc is nil, returns original Iterable.
+func TakeWhile[T any](itb Iterable[T], whileFunc func(v T) bool) Iterable[T] {
+	if itb == nil {
+		return empty[T]()
+	}
+	if whileFunc == nil {
+		return itb
+	}
+	return &takeWhileIterable[T]{itb, whileFunc}
+}
+
+func (itb *takeWhileIterable[T]) Iterator() Iterator[T] {
+	return &takeWhileIterator[T]{itb.itb.Iterator(), itb.whileFunc, false, zero[T]()}
+}
+
+func (it *takeWhileIterator[T]) MoveNext() bool {
+	if it.done {
+		return false
+	}
+	if it.it.MoveNext() && it.whileFunc(it.it.Current()) {
+		it.current = it.it.Current()
+		return true
+	}
+	it.done = true
+	it.current = zero[T]()
+	return false
+}
+
+func (it *takeWhileIterator[T]) Current() T {
+	return it.current
+}
