75d90701e1fea6080a41b73c7777ff55ae7a8063
kitamin <11195207+meian@users.noreply.github.com>
2022-01-30 02:55:32 +0900

    #67 Add orEmpty, isEmpty implements

--- empty.go
+++ empty.go
@@ -11,6 +11,21 @@ func empty[T any]() Iterable[T] {
 	return emptyIterable[T]{}
 }
 
+func orEmpty[T any](itb Iterable[T]) Iterable[T] {
+	if itb == nil {
+		return empty[T]()
+	}
+	return itb
+}
+
+func isEmpty[T any](itb Iterable[T]) bool {
+	if itb == nil {
+		return true
+	}
+	_, ok := itb.(emptyIterable[T])
+	return ok
+}
+
 func (itb emptyIterable[T]) Iterator() Iterator[T] {
 	return emptyIter[T]()
 }
