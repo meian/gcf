2005755984817604bec73ce69920b4e330c4b389
kitamin <11195207+meian@users.noreply.github.com>
2022-02-06 21:58:39 +0900

    #24 Add SkipLastWhile implements

--- skip_last.go
+++ skip_last.go
@@ -13,7 +13,7 @@ type skipLastIterator[T any] struct {
 	iteratorItem[T]
 }
 
-// Skip makes Iterable with elements excepting counted elements from end.
+// SkipLast makes Iterable with elements excepting counted elements from end.
 //
 //   itb := gcf.FromSlice([]{1, 2, 3})
 //   itb = gcf.SkipLast(itb, 2)
@@ -64,6 +64,90 @@ func (it *skipLastIterator[T]) build() {
 		it.it = emptyIter[T]()
 	} else {
 		it.it = makeSliceIterator(s[:len(s)-it.count])
+	}
+	it.built = true
+}
+
+type skipLastWhileIterable[T any] struct {
+	itb       Iterable[T]
+	whileFunc func(v T) bool
+}
+
+type skipLastWhileIterator[T any] struct {
+	it        Iterator[T]
+	whileFunc func(v T) bool
+	built     bool
+	iteratorItem[T]
+}
+
+// SkipLastWhile makes Iterable with elements excepting elements that whileFunc is true from end.
+//
+//   itb := gcf.FromSlice([]{1, 2, 3})
+//   itb = gcf.SkipLastWhile(itb, func(v int) bool { return v <= 2 })
+//
+// If whileFunc is nil, returns original Iterable.
+func SkipLastWhile[T any](itb Iterable[T], whileFunc func(v T) bool) Iterable[T] {
+	if isEmpty(itb) {
+		return orEmpty(itb)
+	}
+	if whileFunc == nil {
+		return itb
+	}
+	return &skipLastWhileIterable[T]{
+		itb:       itb,
+		whileFunc: whileFunc,
+	}
+}
+
+func (itb *skipLastWhileIterable[T]) Iterator() Iterator[T] {
+	return &skipLastWhileIterator[T]{
+		it:        itb.itb.Iterator(),
+		whileFunc: itb.whileFunc,
+	}
+}
+
+func (it *skipLastWhileIterator[T]) MoveNext() bool {
+	if it.done {
+		return false
+	}
+	if !it.built {
+		it.build()
+	}
+	if !it.it.MoveNext() {
+		it.MarkDone()
+		return false
+	}
+	it.current = it.it.Current()
+	return true
+}
+
+func (it *skipLastWhileIterator[T]) Current() T {
+	return it.current
+}
+
+func (it *skipLastWhileIterator[T]) build() {
+	s := iteratorToSlice(it.it)
+	if len(s) == 0 {
+		it.it = emptyIter[T]()
+		it.built = true
+		return
+	}
+	if !it.whileFunc(s[len(s)-1]) {
+		it.it = makeSliceIterator(s)
+		it.built = true
+		return
+	}
+	sLen := len(s)
+	for i := len(s) - 2; i >= 0; i-- {
+		if !it.whileFunc(s[i]) {
+			s = s[:i+1]
+			break
+		}
+	}
+	if len(s) == sLen {
+		it.it = emptyIter[T]()
+	} else {
+		it.it = makeSliceIterator(s)
 	}
 	it.built = true
 }
